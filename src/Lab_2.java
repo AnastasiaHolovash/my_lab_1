8c17b8f81baedb5df879d7dcd2690e10950dc60a
AnastasiaHolovash <nastyuha.holovasch@gmail.com>
2019-03-03 12:56:01 +0200

    2

--- src/Lab_2.java
+++ src/Lab_2.java
@@ -1,58 +1,61 @@
+import java.util.Random;
+
 public class Lab_2 {
     public static void main(String[] args) {
+        final int m = 5;
+        final int n = 3;
 
-        short[][] matrixB = {
-                {-9, 1, 0, 2},
-                { 4, 1, 1, 4},
-                {-2, 2,-1,-5},
-                { 0, 3,-4, 0}
-        };
+        short[][] matrixB = new short[m][n];
+        Random random = new Random();
 
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                //matrixB[i][j] = (short)(random.nextInt(65536) - 32768);
+                matrixB[i][j] = (short)random.nextInt(10);
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 System.out.print(matrixB[i][j] + "\t");
             }
             System.out.println();
         }
 
-        for (int i = 0; i < 4; i++) {
-            for (int j = i+1; j < 4; j++) {
-                short temp = matrixB[i][j];
-                matrixB[i][j] = matrixB[j][i];
-                matrixB[j][i] = temp;
+        short[][] matrixC = new short[n][m];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                matrixC[j][i] = matrixB[i][j];
+
             }
+
         }
 
+        System.out.println();
         System.out.println("Transported matrix:");
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                System.out.print(matrixB[i][j] + "\t");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(matrixC[i][j] + "\t");
             }
             System.out.println();
         }
 
-        short temp1;
-        short temp2;
-        for (int j = 0; j < 4; j++) {
-            if (matrixB[0][j] < matrixB[1][j]){
-                temp1 = matrixB[0][j];
-                temp2 = matrixB[1][j];
-            }else {
-                temp1 = matrixB[1][j];
-                temp2 = matrixB[0][j];
-            }
+        short temp;
+        int sum = 0;
+        for (int j = 0; j < m; j++) {
+            temp = matrixC[0][j];
 
-            for (int i = 2; i < 4; i++) {
-                if (matrixB[i][j] < temp2){
-                    temp2 = matrixB[i][j];
-                }
-                else if (matrixB[i][j] < temp1){
-                    temp2 = temp1;
-                    temp1 = matrixB[i][j];
+            for (int i = 1; i < n; i++) {
+                if (matrixC[i][j] < temp){
+                    temp = matrixC[i][j];
                 }
             }
-            int sum = temp1 + temp2;
-            System.out.println("sum"+ (j+1) + "= " + sum);
+            sum += temp;
+
         }
+        System.out.println();
+        System.out.println("SUM = " + sum);
+
     }
 }
