8205a73c1639d335134352e8653afaee9b8d29de
AnastasiaHolovash <nastyuha.holovasch@gmail.com>
2019-03-03 16:54:33 +0200

    1

--- src/Lab_3.java
+++ src/Lab_3.java
@@ -1,11 +1,12 @@
 import java.util.Arrays;
 import java.util.Collections;
+import java.lang.Character;
 
 public class Lab_3 {
     public static void main(String[] args) {
 
         StringBuffer text = new StringBuffer("Hello it is lucky. I am leader. It is successful! Some words without el");
-        char your_char = 's';
+        char your_char = 'h';
         System.out.println("Text: \n" + text);
 
         String[] arr_words = text.toString().split("[,;:.!?\\s]+");
@@ -18,9 +19,11 @@ public class Lab_3 {
             int count = 0;
             //int count = StringUtils.countMatches("a.b.c.d", ".");
 
+            char your_char_1 = Character.toLowerCase(your_char);
             char[] arr_chars = word.toCharArray();
             for (char chars : arr_chars) {
-                if (chars == your_char) {
+                char chars_1 = Character.toLowerCase(chars);
+                if (chars_1 == your_char_1) {
                     count++;
                 }
             }
